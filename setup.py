994a9a5edee57964fe580b66ddc62f19b4eada6c
Fabian Peter Hammerle <fabian@hammerle.me>
2020-09-02 18:25:22 +0200

    added GPLv3+

--- setup.py
+++ setup.py
@@ -1,3 +1,20 @@
+# python-cc1101 - Python Library to Transmit RF Signals via C1101 Transceivers
+#
+# Copyright (C) 2020 Fabian Peter Hammerle <fabian@hammerle.me>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import pathlib
 
 import setuptools
@@ -8,7 +25,7 @@ setuptools.setup(
     name="cc1101",
     use_scm_version=True,
     packages=setuptools.find_packages(),
-    description="Python Library for CC1101 Transceivers",
+    description="Python Library to Transmit RF Signals via C1101 Transceivers",
     long_description=pathlib.Path(__file__).parent.joinpath("README.md").read_text(),
     long_description_content_type="text/markdown",
     author="Fabian Peter Hammerle",
